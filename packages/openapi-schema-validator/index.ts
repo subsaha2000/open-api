96206cae8806c1edc02089fb997f3ad8c667351e
subsaha2000 <subhendu.saha@droplet.ink>
2026-06-14 19:49:39 +0530

    support 3.1

--- packages/openapi-schema-validator/index.ts
+++ packages/openapi-schema-validator/index.ts
@@ -2,6 +2,7 @@ import ajv, { ValidateFunction, ErrorObject } from 'ajv';
 import addFormats from 'ajv-formats';
 const openapi2Schema = require('./resources/openapi-2.0.json');
 const openapi3Schema = require('./resources/openapi-3.0.json');
+const openapi31Schema = require('./resources/openapi-3.1.json');
 const merge = require('lodash.merge');
 import { IJsonSchema, OpenAPI } from 'openapi-types';
 
@@ -27,12 +28,18 @@ export default class OpenAPISchemaValidator implements IOpenAPISchemaValidator {
   constructor(args: OpenAPISchemaValidatorArgs) {
     const v = new ajv({ allErrors: true, strict: false });
     addFormats(v);
-    const version = (args && parseInt(String(args.version), 10)) || 2;
-    const schema = merge(
-      {},
-      version === 2 ? openapi2Schema : openapi3Schema,
-      args ? args.extensions : {}
-    );
+    let openApiSchema;
+    switch (args.version) {
+      case '3.1':
+        openApiSchema = openapi31Schema;
+        break;
+      default:
+        openApiSchema =
+          (args && parseInt(String(args.version), 10)) || 2 === 2
+            ? openapi2Schema
+            : openapi3Schema;
+    }
+    const schema = merge({}, openApiSchema, args ? args.extensions : {});
     v.addSchema(schema);
     this.validator = v.compile(schema);
   }
